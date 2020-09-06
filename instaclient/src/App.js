eeb372a89cfe87cb0b79f502ebb602d4257fe84f
jade <jadealvares@hotmail.co.uk>
2020-09-06 09:52:15 +0100

    Added data from firebase config
    set up DB on firebase with 1 collection of a username, imageURL and caption and than added config firebase file to repo and called it in useEffect in App.js instead of hard coded useState

--- instaclient/src/App.js
+++ instaclient/src/App.js
@@ -1,24 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import { ReactComponent as Logo} from './Instagram_logo.svg';
 import logo from './insta.png';
 import './App.css';
 import Post from './Post';
 import imageUrl from './me3.png';
+import { db } from './firebase';
 
 function App() {
 
   const [posts, setPosts] = useState([
-    {
-      username: "jade",
-      caption: "here I am",
-      imageUrl: "./me3.png"
-    },
-    {
-      username: "liam",
-      caption: "yaaassssss",
-      imageUrl: "./me3.png"
-    }
+    // {
+    //   username: "jade",
+    //   caption: "here I am",
+    //   imageUrl: "./me3.png"
+    // },
+    // {
+    //   username: "liam",
+    //   caption: "yaaassssss",
+    //   imageUrl: "./me3.png"
+    // }
   ]);
+
+  //runs  apiece of coded based ona  specific condition -> useEffect
+//snapshot is a powerful listeneer.  everytime documents gets modified/changed in posts, snapshot is taken.  everty time new post is added
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => doc.data()));
+    })
+  }, [])
 
   return (
     <div className="App">
@@ -34,7 +43,7 @@ function App() {
           <Post
                 username={post.username}
                 caption={post.caption}
-                imageUrl={posts.imageUrl}
+                imageUrl={post.imageUrl}
           />
         ))
       }
---
+++ instaclient/src/firebase.js
@@ -0,0 +1,23 @@
+// const firebaseConfig = {
+// };
+
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyB4YVfv5VNqFedN9ZLgDhTcuxDHE4DrBgg",
+  authDomain: "jade-instagram.firebaseapp.com",
+  databaseURL: "https://jade-instagram.firebaseio.com",
+  projectId: "jade-instagram",
+  storageBucket: "jade-instagram.appspot.com",
+  messagingSenderId: "164530475236",
+  appId: "1:164530475236:web:2ca324df7f34b6146b1cf1",
+  measurementId: "G-VCCE08ZX03"
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
+
+//export default db;
